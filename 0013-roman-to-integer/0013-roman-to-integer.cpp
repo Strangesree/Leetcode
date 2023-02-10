c52ead954bbb44cb5f3c2199923cf1dfa6d392a9
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-02-10 22:07:37 +0530

    Time: 15 ms (55.51%), Space: 8.9 MB (16.68%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int number=0;
+        unordered_map<char,int> mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        for(int i=0;i<s.size();i++)
+        {
+            if(mp[s[i]]<mp[s[i+1]])
+                number=number-mp[s[i]];
+            else
+                number=number+mp[s[i]];
+        }
+        return number;
+    }
+};
