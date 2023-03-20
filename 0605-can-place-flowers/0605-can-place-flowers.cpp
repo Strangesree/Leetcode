178de7248cee15285f05bdd95ea4b7e37a8eed3c
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-03-20 20:15:44 +0530

    Time: 10 ms (97.73%), Space: 20.3 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& arr, int n) {
-        int previous=1;
+        // int previous=1;
+        // for(int i=0;i<arr.size();i++)
+        // {
+        //     if(previous==1 && arr[i]==0){   
+        //         if(i+1<arr.size() && arr[i+1] == 0){   // for the array out of index problem
+        //             previous=0;
+        //             n--;
+        //         }
+        //         else if(i+1==arr.size()){
+        //             previous=0;
+        //             n--;
+        //         }
+        //     }
+        //     else if(arr[i]==0) previous=1;
+        //     else previous=0;
+        // }
+        // return n<=0;
         for(int i=0;i<arr.size();i++)
         {
-            if(previous==1 && arr[i]==0){   
-                if(i+1<arr.size() && arr[i+1] == 0)   // for the array out of index problem
+            if(arr[i]==0)
+            {
+                bool left=(i==0)||arr[i-1]==0;
+                bool right=(i==arr.size()-1)||arr[i+1]==0;
+                if(left&&right)
                 {
-                    previous=0;
-                    n--;
-                }
-                else if(i+1==arr.size())
-                {
-                    previous=0;
+                    arr[i]=1;
                     n--;
                 }
             }
-            else if(arr[i]==0) 
-                previous=1;
-            else 
-                previous=0;
         }
         return n<=0;
     }
