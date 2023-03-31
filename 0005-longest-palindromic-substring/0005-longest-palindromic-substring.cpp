09e3ec6c1298cab418b2c0cac5807a713ea20046
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-03-31 20:56:28 +0530

    Time: 23 ms (65.93%), Space: 19.2 MB (44.36%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length()<=1){
-            return s;
-        }
-        int max_length=1;
-        int n=s.length();
-        int st=0, en=0;
-        for(int i=0;i<n-1;i++){
-            int l=i ,r=i;
-            while(l>=0 && r<n){
-                if(s[l]==s[r]){
-                    l--;r++;
-                }else{break;}
-                    
+        string s1=""; //Resulting String
+        int len=0;
+        for(int i=0;i<s.length();i++)
+        {
+            //for checking Odd length palindrome
+            int l,r;
+            l=r=i;
+            while(l>=0 && r<s.length() && s[l]==s[r])
+            {   
+                if((r-l+1)>len)
+                {
+                    s1=s.substr(l,r-l+1); //taking the palindrome part
+                    len=r-l+1;
+                }
+                l--;
+                r++;
             }
-            int len=r-l-1;
-            if(len>max_length){
-                max_length=len;
-                st=l+1;
-                en=r-1;   
-            }
-        }
-        for(int i=0;i<n-1;i++){
-            int l=i ,r=i+1;
-            while(l>=0 && r<n){
-                if(s[l]==s[r]){
-                    l--;r++;
-                }else{break;}
-                    
-            }
-            int len=r-l-1;
-            if(len>max_length){
-                max_length=len;
-                st=l+1;
-                en=r-1;            
+
+            //for checking Even length palindrome
+            l=i;
+            r=l+1;
+            while(l>=0 && r<s.length() && s[l]==s[r])
+            {
+                if((r-l+1)>len)
+                {
+                    s1=s.substr(l,r-l+1); //taking the palindrome part
+                    len=r-l+1;
+                }
+                l--;
+                r++;
             }
         }
-        cout<<st<<" "<<en<<" "<<max_length;
-        return s.substr(st,max_length);
+        return s1;
     }
 };
