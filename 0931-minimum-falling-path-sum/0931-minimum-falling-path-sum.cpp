0eb8bad21d8812d6ba21f06da79983d85eba0f77
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-05-05 00:07:26 +0530

    Time: 15 ms (69.59%), Space: 10.3 MB (60.33%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    int helper(vector<vector<int>> &arr,int i,int j,vector<vector<int>> &dp)
-    {
-        if(j<0 || j>arr.size()-1)
-            return 10000;      // in order to get the min if case fails we return maximum.
-        if(i==0)
-            return arr[0][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int up = arr[i][j] + helper(arr,i-1,j,dp);
-        int dialeft = arr[i][j] + helper(arr,i-1,j-1,dp);
-        int diaright = arr[i][j] + helper(arr,i-1,j+1,dp);
-        return dp[i][j] = min(up,min(dialeft,diaright));
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int ans = INT_MAX;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        for(int i=0;i<matrix.size();i++)
-        {
-            ans = min(ans,helper(matrix,n-1,i,dp));
+       int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,0));
+    
+    // Initializing first row - base condition
+    for(int j=0; j<m; j++){
+        dp[0][j] = matrix[0][j];
+    }
+    
+    for(int i=1; i<n; i++){
+        for(int j=0;j<m;j++){
+            
+            int up = matrix[i][j] + dp[i-1][j];
+            
+            int leftDiagonal= matrix[i][j];
+            if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+            else leftDiagonal += 10000;
+            
+            int rightDiagonal = matrix[i][j];
+            if(j+1<m) rightDiagonal += dp[i-1][j+1];
+            else rightDiagonal += 10000;
+            
+            dp[i][j] = min(up, min(leftDiagonal,rightDiagonal));
+            
         }
-        return ans;
-    } 
+    }
+    
+    int maxi = 10000;
+    
+    for(int j=0; j<m;j++){
+        maxi = min(maxi,dp[n-1][j]);
+    }
+    
+    return maxi; 
+    }
 };
