cce19e9c3eb0fa6a07c494dbc8c4d47119258200
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-05-13 22:49:14 +0530

    Time: 347 ms (79.58%), Space: 107.5 MB (19.91%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        const item = JSON.stringify(args);
+        if(item in cache){
+            return cache[item];
+        }
+        cache[item]=fn(...args);
+        return cache[item];
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
