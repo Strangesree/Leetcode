dffb50c30756500e9f0c08235e621c262631e8bd
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-06-06 00:01:58 +0530

    Time: 11 ms (56.86%), Space: 10 MB (63.27%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    bool checkStraightLine(vector<vector<int>>& points) {
-        if(points.size()==2)
-            return true;
-        int n = points.size();
-        int x0 = points[0][0],y0 = points[0][1];
-        int dx = points[1][0] - x0;
-        int dy = points[1][1] - y0;
-        for(int i = 1;i<n;i++){
-            int x = points[i][0];
-            int y = points[i][1];
-            if(dx*(y-y0) != dy*(x-x0))
+    bool checkStraightLine(vector<vector<int>>& coor) {
+        int x = coor[1][0]-coor[0][0],
+            y = coor[1][1]-coor[0][1];
+
+        for(int i = 2; i < coor.size(); i++){
+            if((coor[i][0]-coor[0][0]) * y !=  (coor[i][1]-coor[0][1]) * x)
                 return false;
         }
         return true;
