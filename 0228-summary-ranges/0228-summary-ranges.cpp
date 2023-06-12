cf69d6d8af5ce541c14a13acee4623079896d31e
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-06-12 11:39:31 +0530

    Time: 4 ms (34.63%), Space: 6.9 MB (57.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -5,30 +5,17 @@ public:
         int n = nums.size();
         if(n==0)
             return ans;
-        string temp = to_string(nums[0]);
-        if(n==1){
-            ans.push_back(temp);
-            return ans;
-        }
-        for(int i=1;i<n;i++){
-            if(nums[i-1]+1==nums[i])
-                continue;
-            else{
-                if(to_string(nums[i-1])!=temp)
-                {
-                    temp += "->";
-                    temp += to_string(nums[i-1]);
-                }
-                ans.push_back(temp);
-                temp = to_string(nums[i]);
+        int a = nums[0];
+        for(int i=0;i<n;i++){
+            if(i==n-1 || nums[i]+1 != nums[i+1]){
+                if(nums[i]!= a)
+                    ans.push_back(to_string(a)+"->"+to_string(nums[i]));
+                else
+                    ans.push_back(to_string(a));
+                if(i!= n-1)
+                    a = nums[i+1];
             }
         }
-        if(temp!=""){
-            if(temp != to_string(nums[n-1]))
-                temp += "->"+to_string(nums[n-1]);
-            ans.push_back(temp);
-        }
-            
         return ans;
     
     }
