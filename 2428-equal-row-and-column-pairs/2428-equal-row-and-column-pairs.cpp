c0d50158150b9e7f66dc74b22f81af51c8a6ff95
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-06-14 00:00:45 +0530

    Time: 284 ms (26.81%), Space: 44.3 MB (17.84%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<string,int> rowm;
-        unordered_map<string,int> colm;
-        int n = grid.size();
-        for(int i=0;i<n;i++){
-            string temprow = "";
-            string tempcol = "";
-            for(int j=0;j<n;j++){
-                temprow += to_string(grid[i][j])+"#";
-                tempcol += to_string(grid[j][i])+"#";
-            }
-            rowm[temprow]++;
-            colm[tempcol]++;
+        int count=0;
+        int n=grid.size();
+        unordered_map<string,int> mp;
+        for(int i=0; i<n;i++){
+            string temp="";
+            for(int j:grid[i])
+                temp += to_string(j)+"#";
+            mp[temp]++;
         }
-        int ans = 0;
-        for(auto i:rowm){
-            if(colm[i.first])
-                ans += colm[i.first]*i.second;
+        for(int i=0; i<n;i++){
+            string ans="";
+            //Traversing the matrix column wise and generating new key 
+            for(int j=0; j<n;j++)
+                ans+=to_string(grid[j][i])+"#";
+            // adding the number of occurences of the key generated 
+            count+=mp[ans];
         }
-        return ans;
+        return count;
     }
 };
