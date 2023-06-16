f0955b8768d82e17cefa4c21c7b72c4a4e130cad
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-06-16 21:19:11 +0530

    Time: 268 ms (16.46%), Space: 123.9 MB (73.78%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -2,32 +2,19 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& arr) {
         int n = arr.size();
-        vector<int> pos;
-	    vector<int> neg;
-	    for(int i=0;i<n;i++){
-	        if(arr[i]>=0)
-	            pos.push_back(arr[i]);
-	        else
-	            neg.push_back(arr[i]);
-	    }
-	    int k = 0;
-	    int s = 0;
-	    int i = 0;
-	    while(k<pos.size() && s<neg.size()){
-	        if(i%2==0)
-	            arr[i] = pos[k++];
-	        else
-	            arr[i] = neg[s++];
-	        i++;
-	    }
-	    while(k<pos.size()){
-	        arr[i] = pos[k++];
-	        i++;
-	    }
-	    while(s<neg.size()){
-	        arr[i] = neg[s++];
-	        i++;
-	    }
-			return arr;
+        vector<int> ans(n,0);
+		int p = 0;
+		int ne = 1;
+		for(int i=0;i<n;i++){
+			if(arr[i]>0){
+				ans[p] = arr[i];
+				p = p+2;
+			}
+			else{
+				ans[ne] = arr[i];
+				ne = ne+2;
+			}
 		}
+		return ans;
+	}
 };
