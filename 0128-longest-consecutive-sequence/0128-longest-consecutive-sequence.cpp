52eb432d8a94b0706b587a136c082a070c6d8d49
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-06-18 12:34:14 +0530

    Time: 660 ms (24.13%), Space: 60.7 MB (52.5%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,20 +5,24 @@ public:
         int longest_sequence = 0; 
         for(int i=0; i<nums.size(); i++)
         {
-            if(s.count(nums[i] - 1))
-                continue;   
+            if(s.find(nums[i] - 1)!=s.end())
+                continue;
+   
             else
             {
                 int count = 0;
-                int current_element = nums[i]; 
-                while(s.count(current_element))
+                int current_element = nums[i];
+                
+                while(s.find(current_element) != s.end())
                 {
                     count++;
                     current_element++;
                 }
+                
                 longest_sequence = max(longest_sequence,count);
             }
-        }   
+        }
+        
         return longest_sequence;
     }
 };
