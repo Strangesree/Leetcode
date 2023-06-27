c3c9b939936b28fd943a1ebfa6d0f9e3b577e0f3
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-06-27 21:47:25 +0530

    Time: 209 ms (57.61%), Space: 72.9 MB (60.04%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        // using min heap to store the cost of each candidate in the current window of size k
-        priority_queue<int,vector<int>,greater<int>> pq1; 
-        priority_queue<int,vector<int>,greater<int>> pq2; 
-        long long ans=0; // total cost
-        int l=0,r=costs.size()-1; // left and right pointer of the window
-        int cnt=0; // number of candidates in the current window
-        while(cnt<k){ // add the first k candidates to the window
-            while(pq1.size()<candidates && l<=r) // add the first candidates to the window
-                pq1.push(costs[l++]); 
-            while(pq2.size()<candidates && r>=l) // add the last candidates to the window
-                pq2.push(costs[r--]);
-            
-            int top1 = pq1.size()>0?pq1.top():INT_MAX; // if the first window is full, then the top of the first window is the minimum cost of the first candidates
-            int top2 = pq2.size()>0?pq2.top():INT_MAX; // if the last window is full, then the top of the last window is the minimum cost of the last candidates
-            
-            if(top1<=top2){ // if the first window is cheaper, then add the first candidate to the window
-                ans+=top1;
-                pq1.pop(); 
-            }
-            else{ // if the last window is cheaper, then add the last candidate to the window
-                ans+=top2;
-                pq2.pop();
-            }
-            cnt++; // increase the number of candidates in the window
+        priority_queue<int,vector<int>,greater<int>> left,right;
+        int leftp = 0;
+        for(leftp = 0;leftp <candidates; leftp++){
+            left.push(costs[leftp]);
         }
-        return ans; // return the total cost
+        int n = costs.size();
+        int rightp = n-1;
+        int s = candidates;
+        while(s){
+            if(rightp>leftp){
+                right.push(costs[rightp]);
+                rightp--;
+            }
+            s--;
+        }
+        long long  ans = 0;
+        for(int i=0;i<k;i++){
+            int val = (left.empty()) ? INT_MAX : left.top();
+            int val2 = (right.empty()) ? INT_MAX : right.top();
+            if(val<=val2){
+                ans += val;
+                left.pop();
+                if(leftp<=rightp){
+                    left.push(costs[leftp]);
+                    leftp++;
+                }
+            }
+            else{ 
+                ans += val2;
+                right.pop();
+                if(rightp>=leftp){
+                    right.push(costs[rightp]);
+                    rightp--;
+                }
+            }
+        }
+        return ans;
     }
 };
