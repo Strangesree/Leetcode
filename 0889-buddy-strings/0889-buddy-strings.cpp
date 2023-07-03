c0a18eb99cce527133faa9ab846f92f6c0fa1da3
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-07-03 23:57:20 +0530

    Time: 7 ms (35.87%), Space: 7.1 MB (19.51%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        int n = s.size();
+        int m = goal.size();
+        if(n!=m)
+            return false;
+        if(s == goal){
+            set<char> temp(s.begin(),s.end());
+            return temp.size()<goal.size(); // swapping same characters
+        }
+        int i = 0;
+        int j = n-1;
+        while(i<j && s[i]==goal[i]){
+            i++;
+        }
+        while(j>=0 && s[j]==goal[j]){
+            j--;
+        }
+        if(i<j) 
+            swap(s[i],s[j]);
+        return s==goal;
+    }
+};
