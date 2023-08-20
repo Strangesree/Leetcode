728378d65f8806860e29b8c15e2ccaacfeed9fa4
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-08-20 14:10:20 +0530

    Time: 539 ms (66.67%), Space: 128.7 MB (83.33%) - LeetHub

--- 2979-maximize-the-profit-as-the-salesman/2979-maximize-the-profit-as-the-salesman.cpp
+++ 2979-maximize-the-profit-as-the-salesman/2979-maximize-the-profit-as-the-salesman.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> st;
-    int helper(vector<vector<int>>& of, int n, int index,vector<int>& dp){
-        if(index>=of.size()) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int pick=0,nonPick=0;
-        int val=of[index][1];
-        int nextIndex=upper_bound(st.begin(),st.end(),val)-st.begin();
-        pick=of[index][2]+helper(of,n,nextIndex,dp);
-        nonPick=helper(of,n,index+1,dp);
-        return dp[index]=max(pick,nonPick);
+    int helper(vector<vector<int>>& offer,int ind,vector<int>& dp){
+        if(ind >= offer.size())
+            return 0;
+        if(dp[ind]!=-1) 
+            return dp[ind];
+        int take = 0;
+        int nottake = 0;
+        int val = offer[ind][1];
+        int nextInd = upper_bound(st.begin(),st.end(),val)-st.begin();
+        take = offer[ind][2]+helper(offer,nextInd,dp);
+        nottake = helper(offer,ind+1,dp);
+        return dp[ind] = max(take,nottake);
         
     }
     int maximizeTheProfit(int n, vector<vector<int>>& offers) {
         sort(offers.begin(),offers.end());
         vector<int> dp (offers.size(),-1);
-        for(auto it : offers) st.push_back(it[0]);
-        return helper(offers,n,0,dp);
+        for(auto it : offers) 
+            st.push_back(it[0]);
+        return helper(offers,0,dp);
     }
 };
