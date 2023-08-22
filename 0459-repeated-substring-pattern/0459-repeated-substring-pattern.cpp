52b20b0aabe0cc0f067e8b1bc56e8e5d4f43ceee
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-08-22 23:36:22 +0530

    Time: 31 ms (34.88%), Space: 44.7 MB (28.35%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        return (s+s).substr(1,2*s.size()-2).find(s)!=-1;
+        int n = s.size();
+        for(int i=n/2;i>=1;i--){
+            if(n%i==0){
+                string temp;
+                temp = s.substr(0,i);
+                string temp2;
+                for(int j=1;j<=n/i;j++)
+                    temp2 += temp;
+                if(s==temp2)
+                    return true;
+            }
+        }
+        return false;
     }
 };
