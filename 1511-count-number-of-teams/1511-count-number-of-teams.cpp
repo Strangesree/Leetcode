78dbaf8051b44b69fbaaa1ec31bdb03e9c737ba2
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-08-29 22:58:13 +0530

    Time: 66 ms (88.04%), Space: 9 MB (54.9%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
-	int dp[1002][1002][4];
-	int f1(int i,int prev,int size,vector<int>& rat,int n){
-		if(size==3) return 1;
-		if(i==n){
-			if(size==3) return 1;
-			return 0;
-		}
-		if(dp[i][prev+1][size]!=-1) return dp[i][prev+1][size];
-		int pick=0;
-		if(prev==-1 || rat[i]<rat[prev]) pick=f1(i+1,i,size+1,rat,n);
-		int notpick=f1(i+1,prev,size,rat,n);
-		return dp[i][prev+1][size]=pick+notpick;
-	}
-
-	int f2(int i,int prev,int size,vector<int>& rat,int n){
-		if(size==3) return 1;
-		if(i==n){
-			if(size==3) return 1;
-			return 0;
-		}
-		if(dp[i][prev+1][size]!=-1) return dp[i][prev+1][size];
-		int pick=0;
-		if(prev==-1 || rat[i]>rat[prev]) pick=f2(i+1,i,size+1,rat,n);
-		int notpick=f2(i+1,prev,size,rat,n);
-		return dp[i][prev+1][size]=pick+notpick;
-	}
-
-
-	int numTeams(vector<int>& rat) {
-		int n=rat.size();
-		memset(dp,-1,sizeof(dp));
-		int a=f1(0,-1,0,rat,n);
-		memset(dp,-1,sizeof(dp));
-		int b=f2(0,-1,0,rat,n);
-		return a+b;
-	}
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int teams = 0;
+        for(int u=1;u<n-1;u++){
+            int leftLess=0,leftGreater=0,rightLess=0,rightGreater=0;
+            for(int j=0;j<u;j++){
+                if(rating[j]>rating[u])leftGreater++;
+                else leftLess++;
+            }
+            for(int j=u+1;j<n;j++){
+                if(rating[j]>rating[u])rightGreater++;
+                else rightLess++;
+            }
+            teams += (leftLess*rightGreater) + (leftGreater*rightLess);
+        }
+        return teams;
+    }
 };
