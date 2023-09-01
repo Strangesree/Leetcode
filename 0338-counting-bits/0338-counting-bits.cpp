186fc0cb2c894bdaac280c562caa6851a5816ae2
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-09-01 22:58:34 +0530

    Time: 7 ms (40.85%), Space: 7.9 MB (65.8%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int findcount(int i){
-        int temp = 0;
-        while(i>0){
-            int rem = i%2;
-            if(rem==1)
-                temp++;
-            i = i/2;
-        }
-        return temp;
-    }
+    // int findcount(int i){
+    //     int temp = 0;
+    //     while(i>0){
+    //         int rem = i%2;
+    //         if(rem==1)
+    //             temp++;
+    //         i = i/2;
+    //     }
+    //     return temp;
+    // }
     vector<int> countBits(int n) {
         vector<int> ans(n+1,0);
-        if(n==0){
-            ans[0] = 0;
+        if(n==0)
             return ans;
-        } 
         for(int i=1;i<=n;i++){ 
-            ans[i] = findcount(i);
+            if(i%2==0)
+                ans[i] = ans[i/2];
+            else
+                ans[i] = ans[i-1]+1;
         }
         return ans;
     }
