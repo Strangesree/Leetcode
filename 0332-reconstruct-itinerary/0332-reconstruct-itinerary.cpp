43ccf5cdeca1199b587f1713e7119f178cfd9c33
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-09-16 18:45:11 +0530

    Time: 18 ms (65.21%), Space: 15.2 MB (39.45%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,24 +1,41 @@
-  class Solution {
-      public:
-        map<string, multiset<string>> graph;
-        vector<string> res;
-        void dfs(string vertex) 
-        {
-            while(!graph[vertex].empty())
-            {
-                string to_vertex = *(graph[vertex].begin());
-                graph[vertex].erase(graph[vertex].begin());
-                dfs(to_vertex);
-            }
-            res.push_back(vertex);
+class Solution {
+public:
+    unordered_map<string,vector<string>> adj;
+    vector<string> result;
+    int notickets = 0;
+    bool dfs(string from,vector<string>& path){
+        path.push_back(from);
+        if(path.size()==notickets+1){
+            result = path;
+            return true;
         }
-    
-        vector<string> findItinerary(vector<vector<string>>& tickets) {
-            for (auto ticket : tickets) {
-                graph[ticket[0]].insert(ticket[1]);
-            }
-            dfs("JFK");
-            reverse(res.begin(), res.end());
-            return res;
+        vector<string> &neigh = adj[from];
+        for(int i=0;i<neigh.size();i++){
+            string to = neigh[i];
+            //remove to elimiate cycles
+            neigh.erase(neigh.begin()+i);
+            if(dfs(to,path)==true)
+                return true;
+            neigh.insert(neigh.begin()+i,to); // kinda backtracking
         }
-    };
+        path.pop_back();
+        return false;
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        // here we need to use all edges and it's called eulerian circuit
+        // we need to use all the tickets
+        notickets = tickets.size(); 
+        for(auto &t: tickets){
+            string u = t[0];
+            string v = t[1];
+            adj[u].push_back(v);
+        }
+        //sort the lexical order
+        for(auto &edges: adj){
+            sort(begin(edges.second),end(edges.second));
+        }
+        vector<string> path;
+        dfs("JFK",path);
+        return result;
+    }
+};
