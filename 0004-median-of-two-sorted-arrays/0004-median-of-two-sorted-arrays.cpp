630216f5270e1bd4b801d8f2444dfdd2fa502a47
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-09-21 19:09:19 +0530

    Time: 29 ms (45.31%), Space: 89.5 MB (55.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& num1, vector<int>& num2) {
-        if(num2.size()<num1.size())
-            return findMedianSortedArrays(num2,num1);
-        int n1=num1.size();
-        int n2=num2.size();
-        int low=0;
-        int high=n1;
-        while(low<=high)
-        {
-            int cut1=(low+high)>>1;
-            int cut2=(n1+n2+1)/2-cut1;
-            
-            int left1=cut1==0?INT_MIN:num1[cut1-1];
-            int left2=cut2==0?INT_MIN:num2[cut2-1];
-            int right1=cut1==n1?INT_MAX:num1[cut1];
-            int right2=cut2==n2?INT_MAX:num2[cut2];
-            
-            if(left1<=right2 && left2<=right1)
-            {
-                if((n1+n2)%2==0)
-                    return (max(left1,left2)+min(right1,right2))/2.0;
-                else
-                    return max(left1,left2);
-            }
-            else if(left1>right2)
-                high=cut1-1;
-            else
-                low=cut1+1;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int size = m+n;
+        int even = 0, mid;
+        if(size%2 == 0){
+            even = 1;
+            mid = size/2;
         }
-        return 0.0;
+        else
+            mid = size/2 + 1;
+        int i = 0,j = 0,k = 0,ans = 0;
+        while(k<mid && i<n && j<m){
+            if(nums1[i] <= nums2[j])
+                ans = nums1[i++];
+            else
+                ans = nums2[j++];
+            k++;
+        }
+        while(k<mid){
+            if(i<n)
+                ans = nums1[i++];
+            else 
+                ans = nums2[j++];
+            k++;
+        }
+        if(!even)
+            return (double)ans;
+        else{
+            double x = ans,y;
+            if(i<n && j==m)
+                y = nums1[i];
+            else if(j<m && i==n)
+                y = nums2[j];
+            else if(nums1[i] <= nums2[j])
+                y = nums1[i];
+            else
+                y = nums2[j];
+            
+            return (x+y)/2;
+        }
     }
 };
