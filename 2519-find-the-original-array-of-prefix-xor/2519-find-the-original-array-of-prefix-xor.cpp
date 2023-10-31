5c861cb8f929302ab1bf8fdea81c58dca79a9b6e
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-10-31 23:16:58 +0530

    Time: 101 ms (7.61%), Space: 76.4 MB (58.75%) - LeetHub

--- 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.cpp
@@ -4,7 +4,7 @@ public:
         int n = pref.size();
         vector<int> ans(n);
         for(int i=n-1;i>0;i--){
-            ans[i] = pref[i]^pref[i-1];
+            ans[i] = pref[i] xor pref[i-1];
         }
         ans[0] = pref[0];
         return ans;
