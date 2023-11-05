4f30c8d496baf50da8bcc202e4796c3324fcdc59
Srikanth B <107569061+Strangesree@users.noreply.github.com>
2023-11-05 23:28:56 +0530

    Time: 88 ms (56.48%), Space: 63.4 MB (56.48%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        unordered_map<int,int> mp;
-        int maxi = arr[0];
-        for(int i=1;i<arr.size();i++){
-            maxi = max(maxi,arr[i]);
-            mp[maxi]++;
-            if(mp[maxi]==k)
-                return maxi;
+        // unordered_map<int,int> mp;
+        // int maxi = arr[0];
+        // for(int i=1;i<arr.size();i++){
+        //     maxi = max(maxi,arr[i]);
+        //     mp[maxi]++;
+        //     if(mp[maxi]==k)
+        //         return maxi;
+        // }
+        // return maxi;
+        int maxElement = *max_element(arr.begin(),arr.end());
+        if(k >= arr.size()){
+            return maxElement;
         }
-        return maxi;
+        int winner = arr[0];
+        int streak = 0;
+        int i = 1;
+        while(winner != maxElement){
+            if(winner < arr[i]){
+                winner = arr[i];
+                streak = 1;
+            }
+            else{
+                streak++;
+            }
+            i++;
+            if(streak == k){
+                return winner;
+            }
+        }
+        return winner;
     }
 };
